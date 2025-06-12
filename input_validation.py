4dc1cae6b313bcee62c1b20692f931629b60ac68
Aenard <royoaenard@gmail.com>
2025-06-12 23:59:05 +0800

    1. Added an if-case on all methods to return None once 'q' or 'Q' is entered by the user during initial input.
    2. Removed useless code.

--- input_validation.py
+++ input_validation.py
@@ -1,5 +1,3 @@
-from sys import excepthook
-
 from patient_node import *
 from patient_linkedlist import *
 
@@ -16,28 +14,32 @@ def validate_name():
     while True:
         try:
             """Get a patient name."""
-            name = input("Enter patient name: ").strip()
+            patient_name = input("Enter patient name: ").strip()
 
-            if name == '' or name.isspace():
+            if patient_name == 'q' or patient_name == 'Q':
+                return None
+
+            if patient_name == '' or patient_name.isspace():
                 raise ValueError("Name cannot be empty")
 
-            if not all(c.isalpha() or c.isspace() for c in name):
+            if not all(c.isalpha() or c.isspace() for c in patient_name):
                 raise ValueError("Name must contain only alphabetic characters and spaces")
 
-            if len(name) < 2:
+            if len(patient_name) < 2:
                 raise ValueError("Name must be at least 2 characters long")
             else:
-                return str(name).title()
+                return str(patient_name).title()
         except ValueError as e:
             print(f"Invalid input for name: {e}. Please try again.")
-
-    return str(name).title()
 
 def validate_age():
     """Validate a patient's age."""
     while True:
         try:
             age = input("Enter patient age: ").strip()
+
+            if age == 'q' or age == 'Q':
+                return None
 
             if age == '' or age.isspace():
                 raise ValueError("Age cannot be empty")
@@ -63,6 +65,9 @@ def validate_sickness():
             # Input function just takes whatever the user inputs
             sickness = input(f"\nEnter Patient's sickness: ")
 
+            if sickness == 'q' or sickness == 'Q':
+                return None
+
             # First Condition: Checks whether the input is empty or just spaces,
             # If so, raises a ValueError :D
             if sickness == '' or sickness.isspace():
@@ -70,7 +75,7 @@ def validate_sickness():
 
             if not all(c.isalpha() or c.isspace() for c in sickness):
                 clear_screen()
-                raise ValueError(f"Error: {sickness} contains a numerical character")
+                raise ValueError(f"\nUser entered '{sickness}'. Error: Sickness contains a numerical character")
 
             sickness = str(sickness).title()
 
@@ -81,13 +86,17 @@ def validate_sickness():
                 return sickness
 
         except ValueError as e:
-            print(f"Invalid input for sickness: {e}. Please try again")
+            print(f"Invalid input for sickness!"
+                  f"\n{e}. Please try again")
 
 def validate_urgency_level():
     """Validate a patient's urgency level."""
     while True:
         try:
             urgency_level = input(f"\nEnter Patient's urgency level: ")
+
+            if urgency_level == 'q' or urgency_level == 'Q':
+                return None
 
             if urgency_level == '' or urgency_level.isspace():
                 clear_screen()
@@ -117,152 +126,23 @@ def combine_to_dict(name, age, sickness, urgency_level):
         'urgency_level': urgency_level
     }
 
+def validate_choice(prompt):
+    """Validate user choice."""
+    while True:
+        try:
+            choice = input(f"\n{prompt} (Y/N): ")
 
-# def option1_add_patient():
-#     # Get name
-#     while True:
-#                                                                         # Fixed
-#         try:
-#
-#             # Input function just takes whatever the user inputs
-#             patient_name = input("\nEnter patient name: ")
-#
-#             # First Condition: Checks whether the input is empty or just spaces,
-#             # If so, raises a ValueError :D
-#             if patient_name == '' or patient_name.isspace():
-#                 clear_screen()
-#                 raise ValueError("Name cannot be empty")
-#
-#             # If passed First Condition, safely converts the input to a string
-#             patient_name = str(patient_name).title()
-#
-#             # Second Condition: Checks if all characters contain no numbers
-#             # If so, raises a ValueError, again.
-#             if not all(c.isalpha() or c.isspace() for c in patient_name):
-#                 clear_screen()
-#                 raise ValueError("Name must contain only alphabetic characters and spaces")
-#
-#             # Third Condition: Checks if the input contains at least one alphabetic character
-#             # If so, raises a ValueError, again :P
-#             if len(patient_name) < 2:
-#                 clear_screen()
-#                 raise ValueError("Name must be at least 2 characters long")
-#
-#             # If all conditions are passed, the name is valid
-#             else:
-#                 clear_screen()
-#                 print(f"Please enter a few more details for patient {patient_name}.")
-#                 break
-#         # Default ValueError thing if a user inputs something wild that I didn't anticipate xD
-#         except ValueError as e:
-#             clear_screen()
-#             print(f"Invalid input for name: {e}. Please try again.")
-#
-#     while True:
-#         # Get age                                                                                   #Fixed
-#         try:
-#             print(f"Patient Name: {patient_name}\n")
-#
-#             # Input function just takes whatever the user inputs
-#             print(f"Note: If patient {patient_name}'s age is not known, or is less than a year old, please enter 0.")
-#             age = input(f"Enter Patient {patient_name}'s age: ")
-#
-#             # First Condition: Checks whether the input is empty or just spaces,
-#             # If so, raises a ValueError :D
-#             if age == '' or age.isspace():
-#                 clear_screen()
-#                 raise ValueError("Age cannot be empty")
-#
-#             # Second Condition: Checks whether the input is an integer,
-#             # If it isn't, raises a ValueError :D
-#             elif not age.isdigit():
-#                 clear_screen()
-#                 raise ValueError("Age must be a number")
-#
-#             # If passed both conditions, safely converts the input to an integer
-#             age = int(age)
-#
-#             # Third Condition: Checks if the age is a negative number or greater than 120
-#
-#             if age < 0:
-#                 clear_screen()
-#                 raise ValueError("Age cannot be a negative number")
-#             elif age > 120:
-#                 clear_screen()
-#                 raise ValueError("Age must be less than or equal to 120")
-#             else:
-#                 if age == 0:
-#                     print(f"\nNotification: Patient {patient_name}'s age has been set to 0, indicating unknown or less than a year old")
-#                     print(f"Patient {patient_name}'s age has been set to {age}")
-#                     break
-#                 else:
-#                     clear_screen()
-#                     print(f"Patient {patient_name}'s age has been set to {age}")
-#                 break
-#
-#         except ValueError as e:
-#             print(f"Invalid input for age: {e}. Please try again.")
-#
-#     while True:
-#         # Get sickness                                                                                  #To fix
-#         try:
-#             print(f"Patient Name: {patient_name}\n"
-#                   f"Patient Age: {age}\n")
-#
-#             # Input function just takes whatever the user inputs
-#             sickness = input(f"\nEnter Patient {patient_name}'s sickness: ")
-#
-#             # First Condition: Checks whether the input is empty or just spaces,
-#             # If so, raises a ValueError :D
-#             if sickness == '' or sickness.isspace():
-#                 raise ValueError("Sickness cannot be empty")
-#
-#             if not all(c.isalpha() or c.isspace() for c in sickness):
-#                 clear_screen()
-#                 raise ValueError(f"Error: {sickness} contains a numerical character")
-#
-#             sickness = str(sickness).title()
-#
-#             if len(sickness) < 2:
-#                 clear_screen()
-#                 raise ValueError("Sickness must be at least 2 characters long")
-#             else:
-#                 print(f"DEBUGGING: Sickness: {sickness} is valid.")
-#                 break
-#
-#         except ValueError as e:
-#             print(f"Invalid input for sickness: {e}. Please try again")
-#
-#     while True:
-#         try:
-#             print(f"Patient Name: {patient_name}\n"
-#                   f"Patient Age: {age}\n"
-#                   f"Patient Sickness: {sickness}\n")
-#             urgency_level = input(f"\nEnter Patient {patient_name}'s {sickness} urgency level: ")
-#
-#             if urgency_level == '' or urgency_level.isspace():
-#                 clear_screen()
-#                 raise ValueError("Urgency level cannot be empty")
-#
-#             if not urgency_level.isdigit():
-#                 clear_screen()
-#                 raise ValueError("Urgency level must be a number")
-#
-#             urgency_level = int(urgency_level)
-#
-#             if urgency_level < 1 or urgency_level > 5:
-#                 clear_screen()
-#                 raise ValueError("Urgency level must be between 1 and 5")
-#             else:
-#                 print(f"Urgency Level: {urgency_level} is valid")
-#                 break
-#
-#         except ValueError as e:
-#             print(f"Invalid input for urgency level: {e}. Please try again.")
-#
-#     return {
-#         'name': patient_name,
-#         'age': age,
-#         'sickness': sickness,
-#         'urgency_level': urgency_level
-#     }
+            if choice == '' or choice.isspace():
+                raise ValueError("Choice cannot be empty")
+            if choice.isdigit():
+                raise ValueError("Choice must be a letter, not a number")
+
+            choice = choice.strip().upper()
+
+            if choice == "Y":
+                return choice
+            elif choice == "N":
+                return choice
+
+        except ValueError as e:
+            print(f"Invalid input for choice: {e}. Please try again.")
