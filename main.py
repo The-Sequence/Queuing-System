8b6b49287411b4473b576e4f2f1771c1aa9580fc
Aenard <royoaenard@gmail.com>
2025-06-13 09:58:10 +0800

    1. Revised wording on each menu choice and improved logic.

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ Date: June 13, 2025 (Friday)
 """
 
 from input_validation import *
+from title import *
 
 patient_list = PatientLinkedList()
 
 while True:
+    clear_screen()
     print("***** Queuing System Menu *****\n"
           "1. Add a patient\n"
           "2. Remove a patient\n"
@@ -24,78 +26,93 @@ while True:
           "4. Number of patients in queue\n"
           "Q. Exit\n")
 
-    choice = input("Please enter your choice: ").strip().upper()
+    choice = validate_choice("Please enter your choice", mode=0)
 
-    if choice == '1':
-        print("***** Queuing System - Add a Patient *****\n")
-        patient_name = validate_name()
+    if choice == 1:
+        patient_info = gather_patient_info()
 
-        if patient_name is None: # Returns to the menu as soon as the user enters 'q' or 'Q'
-            continue
-        patient_age = validate_age() # Returns to the menu as soon as the user enters 'q' or 'Q'
-
-        if patient_age is None:
-            continue
-        patient_sickness = validate_sickness() # Returns to the menu as soon as the user enters 'q' or 'Q'
-
-        if patient_sickness is None:
-            continue
-        sickness_urgency_level = validate_urgency_level() # Returns to the menu as soon as the user enters 'q' or 'Q'
-
-        if sickness_urgency_level is None:
+        if patient_info is None:
+            clear_screen()
             continue
 
-        patient_info = combine_to_dict(patient_name, patient_age, patient_sickness, sickness_urgency_level)
-        patient_list.add_patient(patient_info)
+        clear_screen()
+        choice_1_title()
+        print(f"Please confirm the following patient information:\n"
+              f"Name:               {patient_info['name']}\n"
+              f"Age:                {patient_info['age']}\n"
+              f"Sickness:           {patient_info[' ']}\n"
+              f"Urgency Level:      {patient_info['urgency_level']}\n")
 
-        print(f"Patient {patient_name} added successfully.")
-
-        input("Press Enter to continue...")
-
-    elif choice == '2':
-        print("***** Queuing System - Serve a Patient *****\n")
-        # Code to remove a patient
-
-        print(f"The patient next on queue is: {patient_list.peek_patient(mode=1)}")
-        choice = validate_choice("Would you like to serve this patient? ")
-
-        if choice is None:
-            print("Returning to menu...")
-            input("Press Enter to continue...")
+        choice = validate_choice("Would you like to add this patient?")
+        if choice is None or choice == 'N':
+            clear_screen()
+            choice_1_title()
+            print("Patient information discarded.\n")
+            input("Press any key to return to the menu...")
             continue
 
         elif choice == 'Y':
-            patient_list.serve_patient()
-            print(f"Patient {patient_list.peek_patient(mode=0)} has been served and removed from the queue.")
-            input("Press Enter to continue...")
+            clear_screen()
+            choice_1_title()
+            patient_list.add_patient(patient_info)
+            print(f"Patient {patient_info['name']} has been added to the queue.")
+            input("Press any key to return to the menu...")
 
-        elif choice == 'N':
-            print("Returning to menu...")
-            input("Press Enter to continue...")
+        else:
+            break
+
+    elif choice == 2:
+        clear_screen()
+        choice_2_title()
+
+        if patient_list.is_empty():
+            print("There are no patients in the queue.")
+            input("Press any key to return to the menu...")
             continue
+        else:
+            print(f"The patient next on queue is: {patient_list.peek_patient(mode=1)}")
+            choice = validate_choice(f"\nWould you like to serve patient {patient_list.peek_patient()}", mode=1)
+
+        if choice is None or choice == 'N':
+            print("Returning to menu...")
+            input("Press any key to return to the menu...")
+            continue
+
+        elif choice == 'Y':
+            print(f"Patient {patient_list.peek_patient(mode=0)} has been served and is removed from the queue.")
+            patient_list.serve_patient()
+            input("Press any key to return to the menu...")
+
         else:
             continue
 
 
-    elif choice == '3':
-        print("***** Queuing System - Display Patient List *****\n")
+    elif choice == 3:
+        clear_screen()
+        choice_3_title()
         # Code to display patients
 
-        #Note: Not actual implementation
+        # Note: Not actual implementation
         print(patient_list.display_patients())
         input("Press Enter to continue...")
 
-    elif choice == '4':
-        print("***** Queuing System - Number of Patients in Queue *****\n")
+    elif choice == 4:
+        clear_screen()
+        choice_4_title()
         # Code to count patients
 
-        #Note: Not actual implementation
-        print(f"Number of patients in queue: {patient_list.size}")
+        # Note: Not actual implementation
+        if patient_list.is_empty():
+            print("There are no patients in the queue.")
+        else:
+            print(f"Number of patients in queue: {patient_list.size}")
         input("Press Enter to continue...")
 
-    elif choice == 'Q':
+    elif choice == 5:
+        clear_screen()
+        choice_5_title()
         print("Exiting the system. Goodbye!")
         exit(0)
 
     else:
-        raise ValueError("Invalid choice. Please try again.")
+        print("Invalid choice. Please try again.")
