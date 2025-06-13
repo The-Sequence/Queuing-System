83d1485eb931c43888a93d685fd6ac161e61a337
Aenard <royoaenard@gmail.com>
2025-06-13 10:01:01 +0800

    1. Added to minimize clutter in main.py

---
+++ title.py
@@ -0,0 +1,14 @@
+def choice_1_title():
+    print("***** Queuing System - Add a Patient *****\n")
+
+def choice_2_title():
+    print("***** Queuing System - Serve a Patient *****\n")
+
+def choice_3_title():
+    print("***** Queuing System - Display Patients *****\n")
+
+def choice_4_title():
+    print("***** Queuing System - Number of Patients *****\n")
+
+def choice_5_title():
+    print("***** Queuing System - Exit *****\n")
