44dbab58cc1e6f389033dbc55cb913f67ce438c4
Aenard <royoaenard@gmail.com>
2025-06-15 17:11:59 +0800

    Added a new line to notify user that they can choose to go back to menu by pressing Q on any input field.

--- title.py
+++ title.py
@@ -1,14 +1,18 @@
 def choice_1_title():
-    print("***** Queuing System - Add a Patient *****\n")
+    print("***** Queuing System - Add a Patient *****\n"
+          "*** (Press Q and press Enter to cancel) ***\n")
 
 def choice_2_title():
-    print("***** Queuing System - Serve a Patient *****\n")
+    print("***** Queuing System - Serve a Patient *****\n"
+          "*** (Press Q and press Enter to cancel) ***\n")
 
 def choice_3_title():
-    print("***** Queuing System - Display Patients *****\n")
+    print("***** Queuing System - Display Patients *****\n"
+          "*** (Press Q and press Enter to cancel) ***\n")
 
 def choice_4_title():
-    print("***** Queuing System - Number of Patients *****\n")
+    print("***** Queuing System - Number of Patients *****\n"
+          "*** (Press Q and press Enter to cancel) ***\n")
 
 def choice_5_title():
     print("***** Queuing System - Exit *****\n")
